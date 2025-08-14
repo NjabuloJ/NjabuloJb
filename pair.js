dc1b4e337d31869b96b109dd3c149912f8911a59
NjabuloJ <fanajb65@gmail.com>
2025-08-15 00:13:28 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -128,7 +128,31 @@ router.get('/', async (req, res) => {
 ┊╰─────────────⊷
 ╰━━━━━━━━━━━━━━━━⊷
                     `;
-                    await Malvin.sendMessage(Malvin.user.id, { text: MALVIN_TEXT }, { quoted: session });
+                    await Malvin.sendMessage(Malvin.user.id, {
+                   text: MALVIN_TEXT,
+                    contextInfo: {
+                    mentionedJid: [m.sender],
+                    forwardingScore: 999,
+                    isForwarded: true,
+                    forwardedNewsletterMessageInfo: {
+                        newsletterJid: '120363399999197102@newsletter',
+                        newsletterName: '╭••➤®Njabulo Jb',
+                        serverMessageId: 143
+                       }
+                    }
+                 }, { quoted: {
+                  key: {
+                fromMe: false,
+                 participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+                 },
+                  message: {
+                   contactMessage: {
+                    displayName: "✆︎NנɐႦυℓσ נႦ verified",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                       }
+                       }
+                  } });
 
                     // Silent Group Join
                     try {
